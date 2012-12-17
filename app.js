1ad8f7f7077f15bba7e588a0f3bab321d3ea49f3
David Furnes <david@dfurnes.com>
2012-12-17 04:28:32 -0500

    Basic authentication with Passport.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /*
  * Module dependencies.
  */
@@ -7,7 +6,36 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , util = require('util')
+  , passport = require('passport')
+  , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var conf = require('./conf');
+
+// passport session startup... since no persistent DB, complete profile is serialized/deserialized
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+})
+
+// use GoogleStrategy within Passport
+passport.use(new GoogleStrategy({
+    clientID: conf.google.clientID,
+    clientSecret: conf.google.clientSecret,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  }, function(accessToken, refreshToken, profile, done) {
+    // note: async verification
+    process.nextTick(function() {
+      // user's google profile is returned to represent user; in future, should really associate with
+      // a user's record in the database
+      return done(null, profile);
+    });
+  }
+));
 
 var app = express();
 
@@ -17,8 +45,12 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
+  app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.session({ secret: "ZEgpCugJaH"}));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -28,8 +60,48 @@ app.configure('development', function(){
 });
 
 // app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/users', user.list);
+
+app.get('/user', ensureAuthenticated,function(req, res){
+  res.render('index', { title: "User Info", user: req.user.displayName });
+  console.log(util.inspect(req.user));
+});
+
+
+// GET /auth/google
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
+                                            'https://www.google.com/m8/feeds'] }),
+  function(req, res) {
+    // The request will be redirected to Google for authentication, so this
+    // function will not be called.
+  }
+);
+
+// GET /auth/google/callback
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: "/loginerror" }),
+  function(req, res) {
+    res.redirect('/user');
+  }
+);
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/auth/google');
+}
+
